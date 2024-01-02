2b2c3f017c85976d3ec79ee82e54915bd9095c64
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2024-01-02 09:34:57 -0500

    Update main.js

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,4 +1,4 @@
 document.querySelector("form").addEventListener("submit", function(e){
   e.preventDefault();
-  window.location.assign(`/prox/?url=` + document.querySelector("input").value);
+  window.location.assign(`/prox/?url=` + btoa(document.querySelector("input").value);
 });
